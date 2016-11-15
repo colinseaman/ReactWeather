b318e9ce1c22f62eb943c74ff058c4f6f748f2f5
Colin Seaman <colin.seaman@diaku.com>
2016-11-15 06:57:30 -0800

    Initial Commit

---
+++ app/api/openWeatherMap.jsx
@@ -0,0 +1,21 @@
+var axios = require('axios');
+
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&appid=1a03ae0f0d6c1943342d9dca0083209f';
+
+module.exports = {
+  getTemp: function (location) {
+    var encodedLocation = encodeURIComponent(location);
+    var requestUrl = `${OPEN_WEATHER_MAP_URL}&q=${encodedLocation}`;
+
+    return axios.get(requestUrl).then(function (res) {
+      debugger;
+      if (res.data.code && res.data.message) {
+        throw new Error(res.data.message);
+      } else {
+        return res.data.main.temp;
+      }
+    }, function (res) {
+      throw new Error(res.data.message);
+    });
+  }
+}
---
+++ app/components/About.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+/*
+stateless functional component using arrow method
+*/
+
+var About = (props) => {
+  return (
+    <h3>About Component</h3>
+  )
+}
+
+module.exports = About;
