2c51d1c8fb381e3a42f17e83824c077f73d198c7
Colin Seaman <colin.seaman@diaku.com>
2016-11-16 11:09:53 -0800

    Updated styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -7,7 +7,7 @@ stateless functional component using arrow method
 var About = (props) => {
   return (
     <div>
-    <h3>About</h3>
+    <h1 className="text-center">About</h1>
     <p>A simple weather app</p>
     </div>
   )
